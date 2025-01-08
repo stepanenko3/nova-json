da01406d681b363cc9a8887bd1a2d1b060338743
Kotenko Serge <carelloqwerty@gmail.com>
2025-01-08 15:11:29 +0200

    fix nested behave for JsonRepeatable

--- src/Fields/JsonRepeatable.php
+++ src/Fields/JsonRepeatable.php
@@ -28,30 +28,59 @@ class JsonRepeatable extends Field
         return Util::value(data_get($resource, str_replace('->', '.', $attribute)));
     }
 
-    public function fill(NovaRequest $request, $model)
+    public function fill(NovaRequest $request, $model, string $nestedAttribute = null)
     {
-        $model->{$this->attribute} = null;
+        $this->fillModelWithData($model, null, $nestedAttribute ?? $this->attribute);
 
         foreach(($request[$this->attribute] ?? []) as $index => $fieldset) {
             foreach($this->fields as $field) {
+                /** @var Field $field */
+
+                $attributeKeys = [
+                    $index,
+                    $field->attribute,
+                ];
+
+                array_unshift($attributeKeys, $nestedAttribute ?? $this->attribute);
+
+                $attribute = implode('->', $attributeKeys);
+
+                if ($field instanceof self) {
+                    $field->fill(
+                        request: new NovaRequest($request[$this->attribute][$index]),
+                        model: $model,
+                        nestedAttribute: $attribute
+                    );
+
+                    continue;
+                }
+
                 $field->fillAttribute(
                     request: new NovaRequest($request[$this->attribute][$index]),
                     requestAttribute: $field->attribute,
                     model: $model,
-                    attribute: "{$this->attribute}->{$index}->{$field->attribute}",
+                    attribute: $attribute,
                 );
             }
         }
     }
 
-    public function getRules(NovaRequest $request)
+    public function getRules(NovaRequest $request, string $nestedAttribute = null)
     {
         $rules = [
-            $this->attribute => is_callable($this->rules) ? call_user_func($this->rules, $request) : $this->rules,
+            $nestedAttribute ?? $this->attribute => is_callable($this->rules) ? call_user_func($this->rules, $request) : $this->rules,
         ];
 
         foreach($this->fields as $field) {
-            $rules["{$this->attribute}.*.{$field->attribute}"] = is_callable($field->rules) ? call_user_func($field->rules, $request) : $field->rules;
+            $key = $nestedAttribute ?? $this->attribute;
+
+            if ($field instanceof self) {
+                $rules = array_merge($rules, $field->getRules($request, "{$key}.*.{$field->attribute}"));
+
+                continue;
+            }
+
+            $rules["{$key}.*.{$field->attribute}"] = $field->getRules($request)[$field->attribute];
         }
 
         return $rules;
